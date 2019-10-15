23e3b43fb44c5339e7ddd26882ce9735263cde66
igofreely <36579603+igofreely@users.noreply.github.com>
2019-10-15 11:32:09 +0800

    建立项目

---
+++ netauthserver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace netauthserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
